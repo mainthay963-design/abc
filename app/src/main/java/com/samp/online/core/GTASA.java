1f84adfbb1d418ecb54e8653774c761e2db7baa2
mainthay963-design <mainthay963@gmail.com>
2026-05-24 17:18:18 +0700

    Update GTASA.java

--- app/src/main/java/com/samp/online/core/GTASA.java
+++ app/src/main/java/com/samp/online/core/GTASA.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.WindowManager;
+
 import com.wardrumstudios.utils.WarMedia;
 
 public class GTASA extends WarMedia {
@@ -14,8 +16,8 @@ public class GTASA extends WarMedia {
     static {
         vmVersion = null;
         System.out.println("**** Loading SO's");
-        try
-        {
+
+        try {
             vmVersion = System.getProperty("java.vm.version");
             System.out.println("vmVersion " + vmVersion);
             System.loadLibrary("ImmEmulatorJ");
@@ -26,8 +28,7 @@ public class GTASA extends WarMedia {
         System.loadLibrary("samp");
     }
 
-    public static void staticEnterSocialClub()
-    {
+    public static void staticEnterSocialClub() {
         gtasaSelf.EnterSocialClub();
     }
 
@@ -47,79 +48,84 @@ public class GTASA extends WarMedia {
 
     }
 
-    public boolean ServiceAppCommand(String str, String str2)
-    {
+    public boolean ServiceAppCommand(String str, String str2) {
         return false;
     }
 
-    public int ServiceAppCommandValue(String str, String str2)
-    {
+    public int ServiceAppCommandValue(String str, String str2) {
         return 0;
     }
 
     public native void main();
 
-    public void onActivityResult(int i, int i2, Intent intent)
-    {
+    @Override
+    public void onActivityResult(int i, int i2, Intent intent) {
         super.onActivityResult(i, i2, intent);
     }
 
-    public void onConfigurationChanged(Configuration configuration)
-    {
+    @Override
+    public void onConfigurationChanged(Configuration configuration) {
         super.onConfigurationChanged(configuration);
     }
 
-    public void onCreate(Bundle bundle)
-    {
-        if(!once)
-        {
+    @Override
+    public void onCreate(Bundle bundle) {
+        if (!once) {
             once = true;
         }
 
         System.out.println("GTASA onCreate");
+
         gtasaSelf = this;
+
         wantsMultitouch = true;
         wantsAccelerometer = true;
+
         super.onCreate(bundle);
+
+        // FIX KEYBOARD CHE DIALOG
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE
+        );
     }
 
-    public void onDestroy()
-    {
+    @Override
+    public void onDestroy() {
         System.out.println("GTASA onDestroy");
         super.onDestroy();
     }
 
-    public boolean onKeyDown(int i, KeyEvent keyEvent)
-    {
+    @Override
+    public boolean onKeyDown(int i, KeyEvent keyEvent) {
         return super.onKeyDown(i, keyEvent);
     }
 
-    public void onPause()
-    {
+    @Override
+    public void onPause() {
         System.out.println("GTASA onPause");
         super.onPause();
     }
 
-    public void onRestart()
-    {
+    @Override
+    public void onRestart() {
         System.out.println("GTASA onRestart");
         super.onRestart();
     }
 
-    public void onResume()
-    {
+    @Override
+    public void onResume() {
         System.out.println("GTASA onResume");
         super.onResume();
     }
 
-    public void onStart()
-    {
+    @Override
+    public void onStart() {
         System.out.println("GTASA onStart");
         super.onStart();
     }
 
-    public void onStop()
-    {
+    @Override
+    public void onStop() {
         System.out.println("GTASA onStop");
         super.onStop();
     }
